d65551a5fe4ca0bbb3e1801b1b6c92626c650fd3
Xavier Leong <leongxfh@persistent.my>
2015-04-25 12:59:08 +0800

    Complete matrix cache object

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to store matrix and it's inverse
+##
+## The two functions below creates an object for storing matrix
+## and it's inverse, together with cacheSolve which actually do
+## the computation to calculate the inverse matrix.
+## Example usage:
+##
+##   # Create an object to store the matrix
+##   m = makeCacheMatrix( rbind(c(10, 20), c(30, 40)) )
+##
+##   # Calculate the inverse of the matrix,
+##   mI1 = cacheSolve(m)
+##
+##   # Calculating the inverse matrix again will be from the cached results
+##   # hence faster performance.
+##   mI2 = cacheSolve(m)
 
-## Write a short comment describing this function
+
+## makeCacheMatrix -- creates an object to hold a matrix and a cache
+## for its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mI <- NULL
+  set <- function (y) {
+    x <<- y
+    mI <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) mI <<- inverse
+  getInverse <- function() mI
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve -- Perform matrix inverse computation, takes matrix object
+## created from makeCacheMatrix function. If previous computation was done
+## the cached value is return. Print message to indicate whether the result
+## is from cached value or new computation.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mI <- x$getInverse()
+  if(is.null(mI)) {
+    message("solving matrix")
+    mI <- solve(x$get(), ...)
+    x$setInverse(mI)
+  } else {
+    message("getting cached data")
+  }
+  mI
 }
